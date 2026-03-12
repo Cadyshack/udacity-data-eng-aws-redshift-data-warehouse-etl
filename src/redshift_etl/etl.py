a7a953c89478cd78c690f82cdc6f670631a0af3d
Christian Cadieux <cadieux_christian@hotmail.com>
2026-03-12 13:17:37 -0400

    fix client encoding connection issue

--- src/redshift_etl/etl.py
+++ src/redshift_etl/etl.py
@@ -45,7 +45,17 @@ def main():
     DB_PASSWORD = config.get('CLUSTER', 'DB_PASSWORD')
     DB_PORT = config.get('CLUSTER', 'DB_PORT')
 
-    conn = psycopg.connect("host={} dbname={} user={} password={} port={}".format(HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT))
+    # Redshift reports its encoding as 'UNICODE', which psycopg v3
+    # doesn't recognize. Explicitly setting client_encoding='utf8' avoids the
+    # "codec not available in Python: 'UNICODE'" error.
+    conn = psycopg.connect(
+        host=HOST,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        port=DB_PORT,
+        client_encoding='utf8'
+    )
     
     cur = conn.cursor()
     
