d2ff7311f6cc24c73312f6ef72cae598aaeddfbb
Christian Cadieux <cadieux_christian@hotmail.com>
2026-03-12 13:17:57 -0400

    fix table creation order

--- src/redshift_etl/sql/sql_queries.py
+++ src/redshift_etl/sql/sql_queries.py
@@ -200,7 +200,6 @@ song_table_insert = ("""
     WHERE row_num = 1;
 """)
 
-
 artist_table_insert = ("""
     INSERT INTO artists (artist_id, name, location, latitude, longitude)
     SELECT  artist_id,
@@ -235,7 +234,7 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, artist_table_create, time_table_create, song_table_create, songplay_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [user_table_insert, song_table_insert, artist_table_insert, songplay_table_insert, time_table_insert]
